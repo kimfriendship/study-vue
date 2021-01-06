dc19b677b0596de9b8e65ed3d6090e9fe91dfba6
kimfriendship <kfriendship94@gmail.com>
2021-01-06 20:08:05 +0900

    :sparkles: Add router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import router from "./router";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
@@ -15,5 +15,6 @@ Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "./Views/Home";
+import About from "./Views/About";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+  ],
+});
+
+export default router;
