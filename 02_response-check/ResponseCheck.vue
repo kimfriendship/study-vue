8b291cc7b1b102120498131685a307660098109c
kimfriendship <kfriendship94@gmail.com>
2021-01-27 19:49:31 +0900

    :sparkles: Make testing response time

--- 02_response-check/ResponseCheck.vue
+++ 02_response-check/ResponseCheck.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <h1>Response Check</h1>
-    <div id="screen" :class="state">{{ message }}</div>
-    <div>
-      <div>평균 시간: {{}}</div>
+    <div id="screen" :class="state" @click="onClickScreen">
+      {{ message }}
+    </div>
+    <div v-show="result.length">
+      <div>
+        평균 시간:
+        {{ average }}ms
+      </div>
       <button @click="onReset">Reset</button>
     </div>
   </div>
 </template>
 
 <script>
+  let startTime = 0;
+  let endTime = 0;
+  let timeout = null;
+
   export default {
     data() {
       return {
@@ -18,28 +27,58 @@
         message: "click to start",
       };
     },
+    computed: {
+      average() {
+        return this.result.reduce((a, b) => a + b, 0) / this.result.length || 0;
+      },
+    },
     methods: {
-      onReset() {},
+      onReset() {
+        this.result = [];
+      },
+      onClickScreen() {
+        if (this.state === "waiting") {
+          this.state = "ready";
+          this.message = "click when it turns green";
+          timeout = setTimeout(() => {
+            this.state = "now";
+            this.message = "click now!!!!!";
+            startTime = new Date();
+          }, Math.floor(Math.random() * 1000) + 2000);
+        } else if (this.state === "ready") {
+          clearTimeout(timeout);
+          this.state = "waiting";
+          this.message = "you were too quick! click again to start again";
+        } else if (this.state === "now") {
+          endTime = new Date();
+          this.state = "waiting";
+          this.message = "click to start";
+          this.result.push(endTime - startTime);
+        }
+      },
     },
   };
 </script>
 
 <style scoped>
   #screen {
-    width: 300px;
-    height: 300px;
+    box-sizing: border-box;
+    width: 500px;
+    height: 200px;
     text-align: center;
+    padding: 50px;
     font-size: 20px;
+    cursor: pointer;
+    margin-bottom: 20px;
+    color: white;
   }
 
   #screen.waiting {
     background: royalblue;
-    color: white;
   }
 
   #screen.ready {
     background: salmon;
-    color: white;
   }
 
   #screen.now {
--- 02_response-check/webpack.config.js
+++ 02_response-check/webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.join(__dirname, "dist"),
+    publicPath: "/dist",
   },
   mode: "development",
   devtool: "eval",
