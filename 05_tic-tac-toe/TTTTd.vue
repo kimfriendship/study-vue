cc4fd324499b992f71a88e4a683ed9ca61076a54
kimfriendship <kfriendship94@gmail.com>
2021-01-28 18:59:21 +0900

    :sparkles: Use event bus

--- 05_tic-tac-toe/TTTTd.vue
+++ 05_tic-tac-toe/TTTTd.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+  import EventBus from "./EventBus";
   export default {
     props: {
       rowIndex: Number,
@@ -10,52 +11,56 @@
       cell: String,
     },
     methods: {
-      resetData() {
-        const rtData = this.$root.$data;
-        rtData.turnChange = 0;
-        rtData.tableData = [
-          ["", "", ""],
-          ["", "", ""],
-          ["", "", ""],
-        ];
-      },
-      didWin() {
-        const turn = this.$root.$data.turn;
-        const table = this.$root.$data.tableData;
-        const rightDiagonal = [table[0][0], table[1][1], table[2][2]];
-        const leftDiagonal = [table[0][2], table[1][1], table[2][0]];
-        let win = false;
-
-        if (table[this.rowIndex].every((flag) => flag === turn)) win = true;
-        if (table.every((row) => row[this.cellIndex] === turn)) win = true;
-        if (rightDiagonal.every((v) => v === turn)) win = true;
-        if (leftDiagonal.every((v) => v === turn)) win = true;
-
-        return win;
-      },
       onClickCell() {
         if (this.cell !== "") return;
-        const rtData = this.$root.$data;
-        this.$set(
-          rtData.tableData[this.rowIndex],
-          [this.cellIndex],
-          rtData.turn
-        );
-
-        if (this.didWin()) {
-          rtData.winner = rtData.turn;
-          this.resetData();
-        } else {
-          if (rtData.turnChange >= 8) {
-            rtData.winner = "DRAW";
-            this.resetData();
-          } else {
-            rtData.winner = null;
-            rtData.turn = rtData.turn === "O" ? "X" : "O";
-            rtData.turnChange++;
-          }
-        }
+        EventBus.$emit("clickCell", this.rowIndex, this.cellIndex);
       },
+      // resetData() {
+      //   const rtData = this.$root.$data;
+      //   rtData.turnChange = 0;
+      //   rtData.tableData = [
+      //     ["", "", ""],
+      //     ["", "", ""],
+      //     ["", "", ""],
+      //   ];
+      // },
+      // didWin() {
+      //   const turn = this.$root.$data.turn;
+      //   const table = this.$root.$data.tableData;
+      //   const rightDiagonal = [table[0][0], table[1][1], table[2][2]];
+      //   const leftDiagonal = [table[0][2], table[1][1], table[2][0]];
+      //   let win = false;
+
+      //   if (table[this.rowIndex].every((flag) => flag === turn)) win = true;
+      //   if (table.every((row) => row[this.cellIndex] === turn)) win = true;
+      //   if (rightDiagonal.every((v) => v === turn)) win = true;
+      //   if (leftDiagonal.every((v) => v === turn)) win = true;
+
+      //   return win;
+      // },
+      // onClickCell() {
+      //   if (this.cell !== "") return;
+      //   const rtData = this.$root.$data;
+      //   this.$set(
+      //     rtData.tableData[this.rowIndex],
+      //     [this.cellIndex],
+      //     rtData.turn
+      //   );
+
+      //   if (this.didWin()) {
+      //     rtData.winner = rtData.turn;
+      //     this.resetData();
+      //   } else {
+      //     if (rtData.turnChange >= 8) {
+      //       rtData.winner = "DRAW";
+      //       this.resetData();
+      //     } else {
+      //       rtData.winner = null;
+      //       rtData.turn = rtData.turn === "O" ? "X" : "O";
+      //       rtData.turnChange++;
+      //     }
+      //   }
+      // },
     },
   };
 </script>
--- 05_tic-tac-toe/TicTacToe.vue
+++ 05_tic-tac-toe/TicTacToe.vue
@@ -11,6 +11,7 @@
 
 <script>
   import TTTTable from "./TTTTable";
+  import EventBus from "./EventBus";
 
   export default {
     components: {
@@ -27,6 +28,51 @@
           ["", "", ""],
         ],
       };
+    },
+    methods: {
+      resetData() {
+        const rtData = this.$root.$data;
+        rtData.turnChange = 0;
+        rtData.tableData = [
+          ["", "", ""],
+          ["", "", ""],
+          ["", "", ""],
+        ];
+      },
+      didWin(rowIndex, cellIndex) {
+        const turn = this.turn;
+        const table = this.tableData;
+        const rightDiagonal = [table[0][0], table[1][1], table[2][2]];
+        const leftDiagonal = [table[0][2], table[1][1], table[2][0]];
+        let win = false;
+
+        if (table[rowIndex].every((flag) => flag === turn)) win = true;
+        if (table.every((row) => row[cellIndex] === turn)) win = true;
+        if (rightDiagonal.every((v) => v === turn)) win = true;
+        if (leftDiagonal.every((v) => v === turn)) win = true;
+
+        return win;
+      },
+      clickCell(rowIndex, cellIndex) {
+        this.$set(this.tableData[rowIndex], cellIndex, this.turn);
+
+        if (this.didWin(rowIndex, cellIndex)) {
+          this.winner = this.turn;
+          this.resetData();
+        } else {
+          if (this.turnChange >= 8) {
+            this.winner = "DRAW";
+            this.resetData();
+          } else {
+            this.winner = null;
+            this.turn = this.turn === "O" ? "X" : "O";
+            this.turnChange++;
+          }
+        }
+      },
+    },
+    created() {
+      EventBus.$on("clickCell", this.clickCell);
     },
   };
 </script>
