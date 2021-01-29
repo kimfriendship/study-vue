59ed4c57e32089847fdce581ec4d7af38569d75d
kimfriendship <kfriendship94@gmail.com>
2021-01-30 01:58:28 +0900

    :construction: Work on making Trello

---
+++ 07_daily-app/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <app-header :username="username" :is-logged-in="isLoggedIn"></app-header>
+    <main-board v-if="isLoggedIn" :user-data="userData"></main-board>
+    <login-form v-else :username="username" :password="password"></login-form>
+  </div>
+</template>
+
+<script>
+  import AppHeader from "./components/Header";
+  import LoginForm from "./components/LoginForm";
+  import MainBoard from "./components/MainBoard";
+  import usersData from "./userData.js";
+
+  export default {
+    components: {
+      AppHeader,
+      LoginForm,
+      MainBoard,
+    },
+    data() {
+      return {
+        // username: "",
+        username: "aaa",
+        password: "",
+        // isLoggedIn: false,
+        isLoggedIn: true,
+        // userData: {},
+        userData: usersData[0].data,
+      };
+    },
+    methods: {
+      logIn(id, pw) {
+        const userData = usersData.filter(
+          ({ username, password }) => username === id && password === pw
+        );
+        const doesMatch = userData.length !== 0;
+
+        if (doesMatch) {
+          this.userData = userData[0].data;
+          this.isLoggedIn = true;
+        }
+
+        return doesMatch;
+      },
+    },
+  };
+</script>
+
+<style>
+  body {
+    margin: 0;
+    box-sizing: border-box;
+    background: lightgoldenrodyellow;
+  }
+</style>
---
+++ 07_daily-app/src/components/TodoListForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent="addNewList">
+    <input
+      v-model="newTodoListTitle"
+      type="text"
+      placeholder="New Todo List Title"
+    />
+    <button type="submit">Add</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        newTodoListTitle: "",
+      };
+    },
+    methods: {
+      addNewList() {
+        const rootData = this.$root.$children[0].$data.userData;
+        rootData.push({ id: 0, title: this.newTodoListTitle, todoList: [] });
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  form {
+    margin: 30px 50px 0;
+  }
+
+  input {
+    font-size: 18px;
+    border: none;
+    border-bottom: 2px solid steelblue;
+    background: none;
+    margin-right: 20px;
+    height: 30px;
+  }
+
+  button {
+    color: white;
+    background: steelblue;
+    border: none;
+    border-radius: 5px;
+    height: 30px;
+    font-size: 16px;
+    padding: 0 10px;
+  }
+</style>
