f7db4a52cb19872452edc9fee348b88cb8bd8700
kimfriendship <kfriendship94@gmail.com>
2021-01-31 17:28:57 +0900

    :truck: Rename directory

---
+++ 08_trello/src/components/TodoListContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <section v-for="list in userData" :key="list.id" :list="list">
+      <todo-list :title="list.title" :todo-list="list.todoList"></todo-list>
+    </section>
+  </main>
+</template>
+
+<script>
+  import TodoList from "./TodoList";
+
+  export default {
+    components: {
+      TodoList,
+    },
+    props: {
+      userData: {
+        type: Array,
+        default: () => [],
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  main {
+    padding: 100px 50px 0;
+    display: flex;
+    flex-wrap: nowrap;
+    width: 100%;
+    max-width: 100%;
+    height: 65vh;
+    overflow-x: scroll;
+  }
+
+  article {
+    background: steelblue;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-width: 270px;
+    width: 270px;
+    height: 55vh;
+    padding: 30px;
+    border-radius: 10px;
+    margin-right: 50px;
+  }
+</style>
---
+++ 08_trello/src/userData.js
@@ -0,0 +1,19 @@
+const usersData = [
+  {
+    id: 1,
+    username: "aaa",
+    password: "123",
+    data: [
+      {
+        id: 1,
+        title: "Study",
+        todoList: [
+          { id: 1, text: "vue", isDone: false },
+          { id: 2, text: "typescript", isDone: false },
+        ],
+      },
+    ],
+  },
+];
+
+export default usersData;
