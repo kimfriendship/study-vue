e17c3ff45adc15f5f83133252e96311e14e0850f
kimfriendship <kfriendship94@gmail.com>
2021-02-04 11:24:44 +0900

    :sparkles: Implement shopping Chariots

--- TheMajestyOfVue/src/App.vue
+++ TheMajestyOfVue/src/App.vue
@@ -3,17 +3,21 @@
     <One />
     <hr />
     <Two />
+    <hr />
+    <Three />
   </div>
 </template>
 
 <script>
   import One from "./components/practice01/main";
   import Two from "./components/practice02/main";
+  import Three from "./components/practice03/main";
 
   export default {
     components: {
       One,
       Two,
+      Three,
     },
   };
 </script>
---
+++ TheMajestyOfVue/src/components/practice03/ChariotSelection.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h3>"{{ name }}" chariot has {{ count }} horse(s)!</h3>
+    <button @click="onClick" :disabled="checkSelected">
+      {{ buttonMessage }}
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+      },
+      count: {
+        type: Number,
+      },
+      selectedCount: {
+        type: Number,
+      },
+    },
+    computed: {
+      buttonMessage() {
+        switch (true) {
+          case 0 === this.selectedCount:
+            return "Pick Chariot";
+          case this.count > this.selectedCount:
+            return "Hire Horses";
+          case this.count < this.selectedCount:
+            return "Dismiss Horses";
+          case this.count === this.selectedCount:
+            return "Riding!";
+          default:
+            return "";
+        }
+      },
+      checkSelected() {
+        return this.selectedCount === this.count;
+      },
+    },
+    methods: {
+      onClick() {
+        this.$emit("ride", this.count);
+      },
+    },
+  };
+</script>
+
+<style></style>
