7333513ae1c2b45b77c27d6207f42a4d92e2f381
kimfriendship <kfriendship94@gmail.com>
2021-02-12 23:54:32 +0900

    :wrench: Fix configs to use svg loader

--- practice/vue.config.js
+++ practice/vue.config.js
@@ -4,4 +4,16 @@ module.exports = {
       warnings: false,
     },
   },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use("babel-loader")
+      .loader("babel-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+  },
 };
