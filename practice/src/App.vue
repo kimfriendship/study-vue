a9612c54f69220040106f9b7253ba9ce19c0ffcb
kimfriendship <kfriendship94@gmail.com>
2021-02-12 23:21:01 +0900

    :white_check_mark: Test making svg components

--- practice/src/App.vue
+++ practice/src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
-    <Header />
+    <!-- <Header /> -->
+    <CustomIcon :src="Icon" alt="icon" class="icon" />
     <TodoApp />
-    <div id="content" class="content">
+    <!-- <div id="content" class="content">
       <router-view></router-view>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
-  import Header from "./components/Header.vue";
+  // import Header from "./components/Header.vue";
   import TodoApp from "./Views/TodoApp";
+  import CustomIcon from "./components/CustomIcon";
+  import Icon from "./assets/icon.svg";
 
   export default {
     name: "App",
     components: {
-      Header,
+      // Header,
       TodoApp,
+      CustomIcon,
+    },
+    data() {
+      return {
+        Icon,
+      };
     },
   };
 </script>
 
-<style></style>
+<style scoped>
+  .icon path {
+    fill: red;
+  }
+</style>
---
+++ practice/src/components/CustomIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="src" :alt="alt" />
+</template>
+
+<script>
+  export default {
+    props: {
+      src: {
+        type: String,
+      },
+      alt: {
+        type: String,
+      },
+    },
+  };
+</script>
+
+<style></style>
