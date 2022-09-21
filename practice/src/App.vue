27d7eeca08c3c74f3b8993058b65fbf6d10f1d9c
kimfriendship <kfriendship94@gmail.com>
2022-09-21 18:47:25 +0900

    feat: use lottie animation

--- practice/src/App.vue
+++ practice/src/App.vue
@@ -2,18 +2,26 @@
   <main>
     <h1>PDF TEST</h1>
     <section>
-      <Pdfjs />
+      <!-- <Pdfjs /> -->
+      <LottieWebVue
+        auto-play
+        mode="normal"
+        class="lottie"
+        :animation-data="require('./assets/particles.json')"
+      />
     </section>
   </main>
 </template>
 
 <script>
-  import Pdfjs from "./components/Pdfjs.vue";
+  // import Pdfjs from "./components/Pdfjs.vue";
+  import LottieWebVue from "./lottie/lottie-web-vue.vue";
 
   export default {
     name: "App",
     components: {
-      Pdfjs,
+      // Pdfjs,
+      LottieWebVue,
     },
     mounted() {
       console.log("app mounted", window);
