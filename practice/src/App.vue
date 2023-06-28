5993fd9f9d0d189710b43dfce6d4bf10c9116a63
kimfriendship <kfriendship94@gmail.com>
2023-06-29 02:36:44 +0900

    feat: test svg icon

--- practice/src/App.vue
+++ practice/src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <main>
-    <h1>PDF TEST</h1>
+    <h1>ICONS TEST</h1>
     <section>
+      <div>
+        <svg-icon icon-name="check"></svg-icon>
+      </div>
       <!-- <Pdfjs /> -->
-      <LottieWebVue
+      <!-- <LottieWebVue
         auto-play
         mode="normal"
         class="lottie"
         :animation-data="require('./assets/particles.json')"
-      />
+      /> -->
     </section>
   </main>
 </template>
 
 <script>
   // import Pdfjs from "./components/Pdfjs.vue";
-  import LottieWebVue from "./lottie/lottie-web-vue.vue";
+  // import LottieWebVue from "./lottie/lottie-web-vue.vue";
+  // import SvgIcon from "./components/svg/SvgIcon.vue";
+  import SvgIcon from "./components/svg/SvgIcon.vue";
 
   export default {
     name: "App",
     components: {
       // Pdfjs,
-      LottieWebVue,
+      // LottieWebVue,
+      SvgIcon,
     },
-    mounted() {
-      console.log("app mounted", window);
-    },
+    mounted() {},
     methods: {},
   };
 </script>
